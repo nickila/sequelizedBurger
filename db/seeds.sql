66066fe1ff2012d3dfbfc82120ca7573003a42e9
Brian Nickila <brian.nickila@gmail.com>
2019-01-09 10:06:01 -0600

    Uploading original burger app to be modified using sequelize.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom and Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Melt');
