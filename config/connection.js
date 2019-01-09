96d7701ee9dfcf0d1ae168ae598bee3cffa6c0dc
Brian Nickila <brian.nickila@gmail.com>
2019-01-09 11:10:33 -0600

    changed mysql info.

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "coppeR##6531",
+        password: "sesame",
         database: "burgers_db"
     });
 
